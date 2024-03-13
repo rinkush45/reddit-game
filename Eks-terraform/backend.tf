6a131de5548ca9f26ef2a830e82a8c28ac44a5d8
Rinku sharma <rinkusharma.official@yahoo.com>
2024-03-13 10:47:21 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "reddit-game" # Replace with your actual S3 bucket name
+    bucket = "reddit-game1" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-2"
   }
